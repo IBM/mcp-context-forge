c1dcdc35235d409e977aacd19af247e90e66bb77
Frederico Araujo <frederico.araujo@ibm.com>
2025-08-13 16:27:19 -0400

    fix: plugin mcp server initialization
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

---
+++ mcpgateway/plugins/framework/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Plugin framework package.
+
+Copyright 2025
+SPDX-License-Identifier: Apache-2.0
+Authors: Fred Araujo
+
+Exposes plugin framework components:
+- Plugin (base class)
+- PluginManager (loader)
+- Models
+"""
+
+# from mcpgateway.plugins.framework.base import Plugin
+# # from mcpgateway.plugins.framework.external.mcp.server import run_plugin_mcp_server
+# from mcpgateway.plugins.framework.loader.config import ConfigLoader
+# from mcpgateway.plugins.framework.manager import PluginManager
+
+# __all__ = ["ConfigLoader", "Plugin", "PluginManager", "run_plugin_mcp_server"]
