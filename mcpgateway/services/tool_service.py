39716e5670d94341455e25df94e7b9b701e2c6f0
Sebastian <sebastian.iozu@ibm.com>
2025-08-13 09:37:56 +0300

    Mypy for session_registry
    Signed-off-by: Sebastian <sebastian.iozu@ibm.com>

--- mcpgateway/services/tool_service.py
+++ mcpgateway/services/tool_service.py
@@ -150,7 +150,7 @@ class ToolService:
     - Active/inactive tool management.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the tool service.
 
         Examples:
