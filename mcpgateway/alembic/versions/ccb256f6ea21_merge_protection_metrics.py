1f52c7c1582455d6dae9147633f4a3b1e003e3de
Madhavan Kidambi <Madhavan.KidambiVaradan@ibm.com>
2025-09-23 14:14:50 +0530

    Resolving the conflict
    Signed-off-by: Madhavan Kidambi <Madhavan.KidambiVaradan@ibm.com>

--- mcpgateway/alembic/versions/ccb256f6ea21_merge_protection_metrics.py
+++ mcpgateway/alembic/versions/ccb256f6ea21_merge_protection_metrics.py
@@ -1,7 +1,7 @@
 """Merge Protection metrics
 
 Revision ID: ccb256f6ea21
-Revises: 6beda57a5998, e182847d89e6
+Revises: 6beda57a5998, 14ac971cee42
 Create Date: 2025-09-20 15:13:37.220379
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'ccb256f6ea21'
-down_revision: Union[str, Sequence[str], None] = ('6beda57a5998', 'e182847d89e6')
+down_revision: Union[str, Sequence[str], None] = ('6beda57a5998', '14ac971cee42')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
