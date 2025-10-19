970a228aca92f15cdbe457eba315eab5ed7172f0
Mihai Criveti <crivetimihai@gmail.com>
2025-10-19 11:17:13 +0100

    fix: Update gRPC migration to follow pagination migration in chain
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>

--- mcpgateway/alembic/versions/3c89a45f32e5_add_grpc_services_table.py
+++ mcpgateway/alembic/versions/3c89a45f32e5_add_grpc_services_table.py
@@ -15,7 +15,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "3c89a45f32e5"
-down_revision: Union[str, Sequence[str], None] = "2f67b12600b4"
+down_revision: Union[str, Sequence[str], None] = "g1a2b3c4d5e6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
