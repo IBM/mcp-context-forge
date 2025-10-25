07ea9bc8544fcfcba04ef409261ce2a1f74dd774
Mihai Criveti <crivetimihai@gmail.com>
2025-10-25 17:34:58 +0100

    Spec update
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>

--- mcpgateway/config.py
+++ mcpgateway/config.py
@@ -160,7 +160,7 @@ class Settings(BaseSettings):
     app_root_path: str = ""
 
     # Protocol
-    protocol_version: str = "2025-03-26"
+    protocol_version: str = "2025-06-18"
 
     # Authentication
     basic_auth_user: str = "admin"
