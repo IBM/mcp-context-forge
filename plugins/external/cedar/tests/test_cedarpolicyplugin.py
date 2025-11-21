6ac92d98ab1f77957a49229c81f931a670ee31d6
Shriti Priya <shritip@ibm.com>
2025-11-21 14:58:31 -0500

    test cases for all hooks in cedar and custom_dsl policy langauge modes
    Signed-off-by: Shriti Priya <shritip@ibm.com>

--- plugins/external/cedar/tests/test_cedarpolicyplugin.py
+++ plugins/external/cedar/tests/test_cedarpolicyplugin.py
@@ -18,69 +18,8 @@ from mcpgateway.plugins.framework.hooks.tools import ToolPostInvokePayload, Tool
 
 from mcpgateway.common.models import Message, ResourceContent, Role, TextContent, PromptResult
 
-# @pytest.mark.asyncio
-# async def test_cedarpolicyplugin_native_cedar():
-#     """Test plugin prompt prefetch hook."""
-    
-#     policy_config = [
-#         {
-#             'id': 'allow-users-tools', 
-#             'effect': 'Permit', 
-#             'principal': 'Role::"users"', 
-#             'action': ['Action::"get_users"', 'Action::"submit_request"'], 
-#             'resource': [
-#                 {
-#                     "type":"server",
-#                     "name":"hr_services"
-#                     }
-#                 ]
-#         }
-#         # {'id': 'allow-admin-tools','effect': 'Permit','principal': 'Role::"admin"','action':['Action::"get_users"','Action::"submit_request"'],'resource': 'Resource::"tools"'}
-#         ]
-    
-#     config = PluginConfig(
-#         name="test",
-#         kind="cedarpolicyplugin.CedarPolicyPlugin",
-#         hooks=["tool_pre_invoke"],
-#         config={"policy_lang": "cedar","policy" : policy_config },
-#     )
-#     plugin = CedarPolicyPlugin(config)
 
-#     # Test your plugin logic
-#     users = ["bob","alice"]
-#     actions = ["submit_request","get_users"]
-#     for user, action in zip(users,actions):
-#         payload = ToolPreInvokePayload(name=action, args={})
-#         context = PluginContext(global_context=GlobalContext(request_id="1", server_id="hr_services",user=user))
-#         result = await plugin.tool_pre_invoke(payload, context)
-#         assert result.continue_processing
-
-
-# @pytest.mark.asyncio
-# async def test_cedarpolicyplugin_custom_dsl():
-#     """Test plugin prompt prefetch hook."""
-    
-#     policy_config = '[role:admin:agents]\ncustomer_service\napproval\n\n[role:admin:tools]\nget_users\nsubmit_request\n\n[role:users:agents]\ncustomer_service\n\n[role:users:tools]\nget_users\nsubmit_request'
-    
-#     config = PluginConfig(
-#         name="test",
-#         kind="cedarpolicyplugin.CedarPolicyPlugin",
-#         hooks=["tool_pre_invoke"],
-#         config={"policy_lang": "custom_dsl","policy" : policy_config },
-#     )
-#     plugin = CedarPolicyPlugin(config)
-
-#     # Test your plugin logic
-#     users = ["bob","alice"]
-#     actions = ["submit_request","get_users"]
-#     for user, action in zip(users,actions):
-#         payload = ToolPreInvokePayload(name=action, args={})
-#         context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2",user=user))
-#         result = await plugin.tool_pre_invoke(payload, context)
-#         assert result.continue_processing
-
-
-"""This test case is responsible for verifying cedarplugin functionality for all hooks"""
+# """This test case is responsible for verifying cedarplugin functionality for all hooks"""
 @pytest.mark.asyncio
 async def test_cedarpolicyplugin_post_tool_invoke_rbac():
     """Test plugin prompt prefetch hook."""   
@@ -161,10 +100,62 @@ async def test_cedarpolicyplugin_post_tool_invoke_rbac():
     assert redact_count == 1
     assert allow_count == 3
     assert deny_count == 1
+
+"""This test case is responsible for verifying cedarplugin functionality for all hooks"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_post_tool_invoke_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:server/askHR]\nget_leave_balance\nrequest_certificate\n\n[role:employee:agent/employee_agent]\nget_leave_balance\nrequest_certificate\n\n[role:manager:agent/manager_agent]\nget_leave_balance\napprove_leave\npromote_employee\nview_performance\nview_full_output\n\n[role:manager:server/payroll_tool]\nget_leave_balance\napprove_leave\npromote_employee\nview_performance\nview_full_output\n\n[role:hr:server/hr_tool]\nupdate_payroll\nview_performance\nview_full_output\n\n[role:employee:server/payroll_tool]\nview_redacted_output\n\n[role:employee:agent/manager_agent]\nview_redacted_output\n\n[role:employee:server/askHR]\nview_redacted_output'
     
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "\$\d{1,}(,\d{1,})*" }
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "action": "get_leave_balance", "resource": "askHR"},   
+        {"user": "bob", "action": "view_performance", "resource": "payroll_tool"},  
+        {"user": "carol", "action": "update_payroll", "resource": "hr_tool"},    
+        {"user": "alice", "action": "update_payroll", "resource": "hr_tool"},    
+        ]
+    
+    redact_count = 0
+    allow_count = 0
+    deny_count = 0
+    for req in requests:
+        payload = ToolPostInvokePayload(name=req["action"], result={"text": "Alice has a salary of $250,000"})
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id=req["resource"],user=req["user"]))
+        result = await plugin.tool_post_invoke(payload, context)
+        if result.modified_payload and "[REDACTED]" in result.modified_payload.result["text"]:
+            redact_count+=1
+        if result.continue_processing:
+            allow_count+=1
+        if not result.continue_processing:
+            deny_count +=1
+    
+    assert redact_count == 1
+    assert allow_count == 3
+    assert deny_count == 1
+
 # """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
 @pytest.mark.asyncio
-async def test_cedarpolicyplugin_pre_tool_invoke_rbac():
+async def test_cedarpolicyplugin_pre_tool_invoke_cedar_rbac():
     """Test plugin prompt prefetch hook."""   
     policy_config = [
         {
@@ -240,6 +231,52 @@ async def test_cedarpolicyplugin_pre_tool_invoke_rbac():
     assert allow_count == 3
     assert deny_count == 1
 
+# # """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_pre_tool_invoke_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:server/askHR]\nget_leave_balance\nrequest_certificate\n\n[role:employee:agent/employee_agent]\nget_leave_balance\nrequest_certificate\n\n[role:manager:agent/manager_agent]\nget_leave_balance\napprove_leave\npromote_employee\nview_performance\nview_full_output\n\n[role:manager:server/payroll_tool]\nget_leave_balance\napprove_leave\npromote_employee\nview_performance\nview_full_output\n\n[role:hr:server/hr_tool]\nupdate_payroll\nview_performance\nview_full_output\n\n[role:employee:server/payroll_tool]\nview_redacted_output\n\n[role:employee:agent/manager_agent]\nview_redacted_output\n\n[role:employee:server/askHR]\nview_redacted_output'
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "\$\d{1,}(,\d{1,})*" }
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "action": "get_leave_balance", "resource": "askHR"},   
+        {"user": "bob", "action": "view_performance", "resource": "payroll_tool"},  
+        {"user": "carol", "action": "update_payroll", "resource": "hr_tool"},    
+        {"user": "alice", "action": "update_payroll", "resource": "hr_tool"},    
+        ]
+    
+    allow_count = 0
+    deny_count = 0
+    for req in requests:
+        payload = ToolPreInvokePayload(name=req["action"], args={"arg1": "sample arg"})
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id=req["resource"],user=req["user"]))
+        result = await plugin.tool_pre_invoke(payload, context)
+        if result.continue_processing:
+            allow_count+=1
+        if not result.continue_processing:
+            deny_count +=1
+    
+    assert allow_count == 3
+    assert deny_count == 1
 
 """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
 @pytest.mark.asyncio
@@ -307,11 +344,59 @@ async def test_cedarpolicyplugin_prompt_pre_invoke_rbac():
     assert allow_count == 2
     assert deny_count == 1
 
+"""This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_prompt_pre_invoke_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:prompt/judge_prompts]\nview_redacted_output\n\n[role:admin:prompt/judge_prompts]\nview_full_output'
 
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "all" }
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "resource": "judge_prompts"},  #allow
+        {"user": "robert", "resource": "judge_prompts"},  #allow
+        {"user": "carol", "resource": "judge_prompts"}, # deny
+        ]
+    
+    allow_count = 0
+    deny_count = 0
+    
+    for req in requests:
+
+        # Prompt pre hook input
+        payload = PromptPrehookPayload(prompt_id=req["resource"], args={"text": "You are curseword"})
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2",user=req["user"]))
+        result = await plugin.prompt_pre_fetch(payload, context)
+        if result.continue_processing:
+            allow_count+=1
+        if not result.continue_processing:
+            deny_count +=1
+
+    assert allow_count == 2
+    assert deny_count == 1
 
 """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
 @pytest.mark.asyncio
-async def test_cedarpolicyplugin_prompt_post_invoke_rbac():
+async def test_cedarpolicyplugin_prompt_post_invoke_cedar_rbac():
     """Test plugin prompt prefetch hook."""   
     policy_config = [
         {
@@ -382,9 +467,68 @@ async def test_cedarpolicyplugin_prompt_post_invoke_rbac():
     assert deny_count == 1
     assert redact_count == 1
 
+
 """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
 @pytest.mark.asyncio
-async def test_cedarpolicyplugin_resource_pre_fetch_rbac():
+async def test_cedarpolicyplugin_prompt_post_invoke_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:prompt/judge_prompts]\nview_redacted_output\n\n[role:admin:prompt/judge_prompts]\nview_full_output'
+
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "all" }
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "resource": "judge_prompts"},  #allow
+        {"user": "robert", "resource": "judge_prompts"},  #allow
+        {"user": "carol", "resource": "judge_prompts"}, # deny
+        ]
+
+    allow_count = 0
+    deny_count = 0
+    redact_count = 0
+    
+    for req in requests:
+        
+        # Prompt post hook output
+        message = Message(content=TextContent(type="text", text="abc"), role=Role.USER)
+        prompt_result = PromptResult(messages=[message])
+        payload = PromptPosthookPayload(prompt_id=req["resource"], result=prompt_result)
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2",user=req["user"]))
+        result = await plugin.prompt_post_fetch(payload, context)
+        if result.continue_processing:
+            allow_count +=1
+            if result.modified_payload and "[REDACTED]" in result.modified_payload.result.messages[0].content.text:
+                redact_count+=1
+        if not result.continue_processing:
+            deny_count +=1
+       
+    
+    assert allow_count == 2
+    assert deny_count == 1
+    assert redact_count == 1
+
+
+"""This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_resource_pre_fetch_cedar_rbac():
     """Test plugin prompt prefetch hook."""   
     policy_config = [
         {
@@ -452,7 +596,60 @@ async def test_cedarpolicyplugin_resource_pre_fetch_rbac():
 
 """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
 @pytest.mark.asyncio
-async def test_cedarpolicyplugin_resource_post_fetch_rbac():
+async def test_cedarpolicyplugin_resource_pre_fetch_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:resource/https://example.com/data]\nview_redacted_output\n\n[role:admin:resource/https://example.com/data]\nview_full_output'
+
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"}
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "resource": "https://example.com/data"},  #allow
+        {"user": "robert", "resource": "https://example.com/data"},  #allow
+        {"user": "carol", "resource": "https://example.com/data"}, # deny
+        ]
+
+    allow_count = 0
+    deny_count = 0
+    redact_count = 0
+    
+    for req in requests:
+        
+        # Prompt post hook output
+        payload = ResourcePreFetchPayload(uri="https://example.com/data", metadata={})
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2",user=req["user"]))
+        result = await plugin.resource_pre_fetch(payload, context)
+        if result.continue_processing:
+            allow_count +=1
+        if not result.continue_processing:
+            deny_count +=1
+       
+    
+    assert allow_count == 2
+    assert deny_count == 1
+
+
+"""This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_resource_post_fetch_cedar_rbac():
     """Test plugin prompt prefetch hook."""   
     policy_config = [
         {
@@ -480,6 +677,67 @@ async def test_cedarpolicyplugin_resource_post_fetch_rbac():
         hooks=["tool_pre_invoke"],
         config={
             "policy_lang": "cedar",
+            "policy" : policy_config, 
+            "policy_output_keywords": policy_output_keywords, 
+            "policy_redaction_spec": policy_redaction_spec
+            },
+    )
+    plugin = CedarPolicyPlugin(config)
+    info = {
+            "alice": "employee",
+            "bob": "manager",
+            "carol": "hr",
+            "robert": "admin"
+        }
+    plugin._set_jwt_info(info)
+    requests = [
+        {"user": "alice", "resource": "https://example.com/data"},  #allow
+        {"user": "robert", "resource": "https://example.com/data"},  #allow
+        {"user": "carol", "resource": "https://example.com/data"}, # deny
+        ]
+
+    allow_count = 0
+    deny_count = 0
+    redact_count = 0
+    
+    for req in requests:
+        
+        # Prompt post hook output
+        content = ResourceContent(
+            type="resource",
+            uri="test://large",
+            text="test://abc@example.com",
+            id="1"
+            )
+        payload = ResourcePostFetchPayload(uri="https://example.com/data", content=content)
+        context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2",user=req["user"]))
+        result = await plugin.resource_post_fetch(payload, context)
+        if result.continue_processing:
+            allow_count +=1
+            if result.modified_payload and "[REDACTED]" in result.modified_payload.content.text:
+                redact_count+=1
+        if not result.continue_processing:
+            deny_count +=1
+       
+    
+    assert allow_count == 2
+    assert deny_count == 1
+    assert redact_count == 1
+
+
+# # """This test case is responsible for verifying cedarplugin functionality for tool pre invoke"""
+@pytest.mark.asyncio
+async def test_cedarpolicyplugin_resource_post_fetch_custom_dsl_rbac():
+    """Test plugin prompt prefetch hook."""   
+    policy_config = '[role:employee:resource/https://example.com/data]\nview_redacted_output\n\n[role:admin:resource/https://example.com/data]\nview_full_output'
+    policy_output_keywords = {"view_full": "view_full_output","view_redacted": "view_redacted_output"}
+    policy_redaction_spec = {"pattern":  "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"}
+    config = PluginConfig(
+        name="test",
+        kind="cedarpolicyplugin.CedarPolicyPlugin",
+        hooks=["tool_pre_invoke"],
+        config={
+            "policy_lang": "custom_dsl",
             "policy" : policy_config, 
             "policy_output_keywords": policy_output_keywords, 
             "policy_redaction_spec": policy_redaction_spec
