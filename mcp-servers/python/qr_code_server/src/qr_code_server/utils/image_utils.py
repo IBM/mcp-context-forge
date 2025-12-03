e5908aaaaa775ba22cb9d53a94a465c31338e142
cafalchio <mcafalchio@gmail.com>
2025-12-03 09:13:53 +0000

    Improved image utils to handle batch qr code generation     - Decoupled qr image generation from the create_qr_code     - Improved Ascii save to handle BytesIO for batch zip generation

--- mcp-servers/python/qr_code_server/src/qr_code_server/utils/image_utils.py
+++ mcp-servers/python/qr_code_server/src/qr_code_server/utils/image_utils.py
@@ -1,13 +1,17 @@
 
+from io import BytesIO, TextIOBase
 import numpy as np
 import qrcode
+from qrcode.image.pil import PilImage
+from qrcode.image.svg import SvgImage
+from qrcode.image.base import BaseImage
 
 
 class SaveImageError(Exception):
     pass
 
 
-class ImageAscii(qrcode.image.base.BaseImage):
+class ImageAscii(BaseImage):
 
     def __init__(self, border, width, box_size, qrcode_modules=None, **kwargs):
         self.border = border
@@ -21,10 +25,62 @@ class ImageAscii(qrcode.image.base.BaseImage):
         mapped = block[img_arr.astype(int)]
         return "\n".join("".join(row) for row in mapped)
 
-    def save(self, path):
+    def save(self, target):
         ascii_qr = self.to_string()
+        if isinstance(target, BytesIO):
+            try:
+                target.write(ascii_qr.encode("utf-8"))
+                return
+            except OSError as e:
+                raise SaveImageError(f"Error saving ASCII image: {e}") from e
         try:
-            with open(path, "w", encoding="utf-8") as f:
+            with open(target, "w", encoding="utf-8") as f:
                 f.write(ascii_qr)
         except Exception as e:
             raise SaveImageError(f"Error saving ASCII image: {e}") from e
+
+
+def create_qr_image(
+    data: str,
+    format: str = "png",
+    size: int = 10,
+    border: int = 4,
+    error_correction: str = "M",
+    fill_color: str = "black",
+    back_color: str = "white",
+
+) -> BaseImage:
+    """Create a QR code image.
+    """
+
+    ec_map = {
+        "L": qrcode.constants.ERROR_CORRECT_L,
+        "M": qrcode.constants.ERROR_CORRECT_M,
+        "Q": qrcode.constants.ERROR_CORRECT_Q,
+        "H": qrcode.constants.ERROR_CORRECT_H,
+    }
+    format = "txt" if format == "ascii" else format
+
+    factory_map = {
+        "png": PilImage,
+        "svg": SvgImage,
+        "txt": ImageAscii
+    }
+
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=ec_map[error_correction],
+        box_size=size,
+        border=border,
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    factory = factory_map.get(format, PilImage)
+
+    return qr.make_image(
+        image_factory=factory,
+        fill_color=fill_color,
+        back_color=back_color,
+    )
