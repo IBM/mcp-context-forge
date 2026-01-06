092cc896b8ae0eaf5dbaff6ccd056929bcc01f17
Aidan Butler <aidbutlr@ie.ibm.com>
2026-01-06 17:25:53 +0000

    CYFR-280 postgres support

--- .sps/unit_test.sh
+++ .sps/unit_test.sh
@@ -14,7 +14,9 @@ make install-dev
 echo "############# Running Install psycogpg ################"
 . $HOME/.venv/mcpgateway/bin/activate && \
     python3 -m uv pip install 'psycopg[c]' && \
-    python3 -m uv pip install 'psycopg2'
+    python3 -m uv pip install 'psycopg2' && \
+    python3 -m uv pip install 'pytest' && \
+    python3 -m uv pip install 'pytest_cov' 
 echo "############# Running Install DB ################"
 make install-db
 #echo "############# Running Linting ################"
