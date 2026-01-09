396cd32e94dd3c1d30c92ba6530718e15ba2c7f6
Aidan Butler <aidbutlr@ie.ibm.com>
2026-01-09 22:22:14 +0000

    CYFR-280 adding first smoke tests

--- .sps/run_smoke_test.sh
+++ .sps/run_smoke_test.sh
@@ -4,7 +4,8 @@ cd cyberfraud-mcp-management-service
 echo "#############################"
 echo "Running smoke tests"
 echo "#############################"
-pip3 install uv
+pip3 install --upgrade pip
+pip3 install uv --user
 uv run pytest tests/integration -v -s --setup-show
 if [ $? != 0 ]; then
   echo "Integration test failed, exiting";
