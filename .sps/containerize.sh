ea70df9a46a31c79e2ce4fb05bffb77ed21427fd
Aidan Butler <aidbutlr@ie.ibm.com>
2026-01-12 14:13:40 +0000

    CYFR-280 adding first smoke tests

--- .sps/containerize.sh
+++ .sps/containerize.sh
@@ -47,7 +47,7 @@ make IMAGE_TAG="base" docker-prod
     docker tag "mcpgateway/mcpgateway:${IMAGE_TAG}" "${IMAGE}" && \
     docker push "${IMAGE}"
 
-MCP_GATEWAY_IMAGE="${IMAGE}"
+MCP_GATEWAY_IMAGE_TAG="${IMAGE_TAG}"
 RUN_SMOKE_TESTS=$(get_env run-smoke-tests "1")
 if [ $RUN_SMOKE_TESTS == "1" ]; then
    source ./.sps/run_smoke_test.sh
