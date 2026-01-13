5cede89202323d5a1034182d1b9f89d6896be496
Aidan Butler <aidbutlr@ie.ibm.com>
2026-01-13 20:06:45 +0000

    CYFR-280 multi-arch image support

--- .sps/containerize.sh
+++ .sps/containerize.sh
@@ -54,7 +54,6 @@ if [ $MULTI_ARCH_BUILD == "1" ]; then
    sed -i "s%BASE_IMAGE_REPO%${BASE_IMAGE_REPO}%g" Containerfile.cyberfraud
    make REGISTRY="${DOCKER_REGISTRY}" IMAGE_BASE="$IMAGE_BASE" IMAGE_TAG="$BASE_IMAGE_TAG" CONTAINER_RUNTIME=docker CONTAINER_FILE=./Containerfile.lite  container-build-multi && \
    make REGISTRY="${DOCKER_REGISTRY}" IMAGE_BASE="${IMAGE_NAME}" IMAGE_TAG="${IMAGE_TAG}" CONTAINER_RUNTIME=docker CONTAINER_FILE=./Containerfile.cyberfraud  container-build-multi && \
-   docker push "${IMAGE}"
 else
    echo "Building single architecture image"
    BASE_IMAGE_REPO="${IMAGE_BASE}:${BASE_IMAGE_TAG}"
