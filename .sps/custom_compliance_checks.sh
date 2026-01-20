7a3bf2ea024a383e152e26a677be1d17d1c65969
Aidan Butler <aidbutlr@ie.ibm.com>
2026-01-20 15:08:54 +0000

    CYFR-40335 fix compliance scans

--- .sps/custom_compliance_checks.sh
+++ .sps/custom_compliance_checks.sh
@@ -2,7 +2,7 @@
 
 source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/utilities/python_utils.sh
 install_python3 3.11
-pip3.11 install --upgrade pip pytest pytest-cov sqlalchemy
+pip3.11 install --upgrade pip 
 mkdir -p app
 echo "############# Python Version #################"
 python3 -V
