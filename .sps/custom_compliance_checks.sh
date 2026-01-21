9d8af059f99908b956423c50d8ebc3b4425e9ec0
AIDAN BUTLER <AIDBUTLR@ie.ibm.com>
2026-01-21 09:22:28 +0000

    CYFR-40335  Update custom_compliance_checks.sh

--- .sps/custom_compliance_checks.sh
+++ .sps/custom_compliance_checks.sh
@@ -2,9 +2,7 @@
 
 export PATH=/root/.local/bin:$PATH
 source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/utilities/python_utils.sh
-source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/utilities/go_utils.sh
 install_python3 3.11
-install_go
 pip3.11 install --upgrade pip 
 mkdir -p app
 echo "############# Python Version #################"
