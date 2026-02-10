ce6ac4299c78ce54ef43d79b53a40b85b95d28f7
Ayo Kukoyi <kukoyio@tcd.ie>
2026-02-10 17:07:44 +0000

    Source Scanner plugin scaffold with Semgrep integration
    - Plugin skeleton and registration
    - Semgrep CLI invocation and finding model
    - Policy evaluation and config normalization
    - Initial tests and documentation
    
    NOTE:
    - Bandit, normalizer, and language detection are intentionally incomplete
    - exec.run_command integration and severity normalization will follow
    
    Signed-off-by: Ayo Kukoyi <kukoyio@tcd.ie>

---
+++ plugins/source_scanner/utils/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Location: ./plugins/source_scanner/utils/__init__.py
+Copyright 2025
+SPDX-License-Identifier: Apache-2.0
+Authors: Ayo
+
+Utils package
+"""
