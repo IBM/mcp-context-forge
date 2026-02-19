9077eb388c3772229c3e3f15d6989346c6728f19
Keval Mahajan <mahajankeval23@gmail.com>
2026-02-19 16:05:12 +0530

    use internal rpc url for proxy related use cases
    Signed-off-by: Keval Mahajan <mahajankeval23@gmail.com>

--- mcpgateway/transports/streamablehttp_transport.py
+++ mcpgateway/transports/streamablehttp_transport.py
@@ -1869,7 +1869,7 @@ class SessionManagerWrapper:
                         rpc_headers["authorization"] = headers["authorization"]
 
                     response = await client.post(
-                        f"http://127.0.0.1:{settings.port}/rpc",
+                        settings.internal_rpc_url,
                         content=body,
                         headers=rpc_headers,
                         timeout=30.0,
@@ -2020,7 +2020,7 @@ class SessionManagerWrapper:
                                 rpc_headers["authorization"] = headers["authorization"]
 
                             response = await client.post(
-                                f"http://127.0.0.1:{settings.port}/rpc",
+                                settings.internal_rpc_url,
                                 content=body,
                                 headers=rpc_headers,
                                 timeout=30.0,
