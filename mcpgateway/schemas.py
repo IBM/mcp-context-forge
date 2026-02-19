f3ed0ecb732f45a840be7d6d0236158387e244d5
Rakhi Dutta <rakhibiswas@yahoo.com>
2026-02-19 19:05:17 +0530

    edittool
    Signed-off-by: Rakhi Dutta <rakhibiswas@yahoo.com>

--- mcpgateway/schemas.py
+++ mcpgateway/schemas.py
@@ -1340,6 +1340,9 @@ class ToolUpdate(BaseModelWithConfigDict):
         # For ToolUpdate: Force re-population if URL changed
         if url_changed:
             logger.info("URL changed in ToolUpdate, forcing schema re-population from OpenAPI spec")
+            # Clear existing schemas to force fresh population from new URL
+            values["input_schema"] = None
+            values["output_schema"] = None
             input_needs_population = True
             output_needs_population = True
         else:
