152c3f524c4a101b5614b9b45830789199886e69
Akshay Shinde <akshay.shinde26@ibm.com>
2026-02-27 14:39:26 +0000

    test: add regression tests to ensure audit_trail.log_action does not receive db session
    Signed-off-by: Akshay Shinde <akshay.shinde26@ibm.com>

--- mcpgateway/services/prompt_service.py
+++ mcpgateway/services/prompt_service.py
@@ -2112,7 +2112,6 @@ class PromptService(BaseService):
                         "arguments_provided": arguments_supplied,
                         "request_id": request_id,
                     },
-                    db=db,
                 )
 
                 structured_logger.log(
