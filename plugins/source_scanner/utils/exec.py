1b20a3292dc31e59a742e0cbeb0a47307023c374
Xinyi <chenx10@tcd.iegit>
2026-03-10 20:56:45 +0000

    feat 2235: fix bugs for pipeline

--- plugins/source_scanner/utils/exec.py
+++ plugins/source_scanner/utils/exec.py
@@ -6,8 +6,3 @@ SPDX-License-Identifier: Apache-2.0
 Authors: Xinyi, Ayo
 
 Re-exports run_command and ExecResult from mcpgateway.utils.exec for backwards compatibility.
-"""
-
-from mcpgateway.utils.exec import run_command, ExecResult
-
-__all__ = ["run_command", "ExecResult"]
