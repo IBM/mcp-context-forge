73c6171cfb25e8407885e6d291b7b8af7a948283
Xinyi <chenx10@tcd.iegit>
2026-03-10 21:08:20 +0000

    feat 2235: correct down_revision for image signing migration

--- mcpgateway/alembic/versions/9b680492ec77_add_image_signing_tables.py
+++ mcpgateway/alembic/versions/9b680492ec77_add_image_signing_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '9b680492ec77'
-down_revision: Union[str, Sequence[str], None] = '2612ebdfb9f0'
+down_revision: Union[str, Sequence[str], None] = 'f1a2b3c4d5e6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
