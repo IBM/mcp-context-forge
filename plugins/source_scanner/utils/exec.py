abf71c635fd6335db7ad41be49ab7f60f05ba172
Xinyi <chenx10@tcd.iegit>
2026-03-11 15:09:16 +0000

    feat 2235: delete unnecessary files

--- plugins/source_scanner/utils/exec.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Location: ./plugins/source_scanner/utils/exec.py
-Copyright 2025
-SPDX-License-Identifier: Apache-2.0
-Authors: Xinyi, Ayo
-
-Re-exports run_command and ExecResult from mcpgateway.utils.exec for backwards compatibility.
