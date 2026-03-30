e8ae721251a4a2487a49b52f66b63439e1f0bb5e
Agnetha <agnethakamath@gmail.com>
2026-03-30 22:33:09 +0100

    issue 2216: recommitting files for container_scanner plugin, along with additional files for UI and server

---
+++ plugins/container_scanner/auth/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Location: ./plugins/container_scanner/auth/__init__.py
+Copyright 2026
+SPDX-License-Identifier: Apache-2.0
+Authors: Agnetha
+
+Public API for the auth subpackage.
+"""
+
+from __future__ import annotations
