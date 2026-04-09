7590dd95b079e1a0aef22501e2599ea325091d35
Gabriel Costa <gabrielcg@proton.me>
2026-04-09 12:12:18 +0100

    Scaffold i18n
    Signed-off-by: Gabriel Costa <gabrielcg@proton.me>

---
+++ client/src/i18n/locales/pt-BR/index.ts
@@ -0,0 +1,11 @@
+import common from './common.json';
+import navigation from './navigation.json';
+import auth from './auth.json';
+import dashboard from './dashboard.json';
+
+export default {
+  ...common,
+  ...navigation,
+  ...auth,
+  ...dashboard,
+};
