a6db8b0db6045612d500a274dc908afba440b86f
lucarlig <luca.carlig@ibm.com>
2026-04-21 17:30:36 +0100

    test: normalize rust stub signature assertion
    Signed-off-by: lucarlig <luca.carlig@ibm.com>

--- tests/unit/test_validation_middleware_rust_stubs.py
+++ tests/unit/test_validation_middleware_rust_stubs.py
@@ -6,7 +6,13 @@ STUB_PATH = ROOT / "crates" / "validation_middleware_rust" / "python" / "validat
 
 def test_validation_middleware_rust_generates_packaged_stub():
     stub = STUB_PATH.read_text(encoding="utf-8")
+    normalized_stub = " ".join(stub.split())
+    expected_signature = (
+        "def validate_http_request( self, parameters: typing.Sequence[tuple[builtins.str, builtins.str]], "
+        "content_type: builtins.str, raw_body: bytes | None = None, skip_parameter_validation: builtins.bool = False, "
+        ") -> typing.Optional[tuple[builtins.str, builtins.str]]: ..."
+    )
 
     assert "class Validator:" in stub
-    assert "def validate_http_request" in stub
+    assert expected_signature in normalized_stub
     assert "def validate_resource_path" in stub
