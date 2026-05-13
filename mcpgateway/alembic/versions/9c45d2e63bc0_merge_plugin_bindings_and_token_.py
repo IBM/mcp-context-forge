983694eefc05ab7d1212e8e6be097e52c673da32
Brian Hussey <brian.hussey@ie.ibm.com>
2026-05-13 10:09:00 +0100

    Cleaning alembic issue with v1.0.0 alembic_version
    Signed-off-by: Brian Hussey <brian.hussey@ie.ibm.com>

--- mcpgateway/alembic/versions/9c45d2e63bc0_merge_plugin_bindings_and_token_.py
+++ mcpgateway/alembic/versions/9c45d2e63bc0_merge_plugin_bindings_and_token_.py
@@ -7,7 +7,7 @@ Authors: Mihai Criveti
 merge plugin_bindings and token_revocation heads
 
 Revision ID: 9c45d2e63bc0
-Revises: 4842b831d24e, c3c3b7f9b014
+Revises: 4842b831d24e
 Create Date: 2026-05-01 00:35:39.894249
 """
 
@@ -16,7 +16,7 @@ from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
 revision: str = "9c45d2e63bc0"  # pragma: allowlist secret
-down_revision: Union[str, Sequence[str], None] = ("4842b831d24e", "c3c3b7f9b014")  # pragma: allowlist secret
+down_revision: Union[str, Sequence[str], None] = "4842b831d24e"  # pragma: allowlist secret
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
