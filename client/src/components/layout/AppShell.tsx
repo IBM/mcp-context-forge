da121d7bf571a30030689b015384e8c1a8df4b31
Clare Byrne <clare.byrne1@ibm.com>
2026-05-18 17:16:58 +0100

    restores sidebar layout
    Signed-off-by: Clare Byrne <clare.byrne1@ibm.com>

--- client/src/components/layout/AppShell.tsx
+++ client/src/components/layout/AppShell.tsx
@@ -5,14 +5,12 @@ import { Header } from "./Header";
 
 export function AppShell({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider className="flex min-h-svh flex-col">
-      <Header />
-      <div className="flex min-h-0 flex-1">
-        <AppSidebar />
-        <SidebarInset className="min-h-0">
-          <div className="flex flex-1 flex-col gap-4 p-6">{children}</div>
-        </SidebarInset>
-      </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        <div className="flex flex-1 flex-col gap-4 p-6">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
