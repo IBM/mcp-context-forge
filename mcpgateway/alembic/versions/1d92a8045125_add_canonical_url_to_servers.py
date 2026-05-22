aa6439309ca25b80e05bb95798f4c137c90f76c0
Mohan Lakshmaiah <mohan.economist@gmail.com>
2026-05-22 20:28:17 +0530

    chore: add pragma allowlist secret to migration revision identifiers
    Signed-off-by: Mohan Lakshmaiah <mohan.economist@gmail.com>

--- mcpgateway/alembic/versions/1d92a8045125_add_canonical_url_to_servers.py
+++ mcpgateway/alembic/versions/1d92a8045125_add_canonical_url_to_servers.py
@@ -14,8 +14,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "1d92a8045125"
-down_revision: Union[str, Sequence[str], None] = "e28566875fa4"
+revision: str = "1d92a8045125"  # pragma: allowlist secret
+down_revision: Union[str, Sequence[str], None] = "e28566875fa4"  # pragma: allowlist secret
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
