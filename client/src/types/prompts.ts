22525d63b1b6b62a31e4fa875fe8bc21feb467fe
Clare Byrne <clare.byrne1@ibm.com>
2026-07-02 12:20:10 +0100

    style: align prompts page cards with tools
    Signed-off-by: Clare Byrne <clare.byrne1@ibm.com>

---
+++ client/src/types/prompts.ts
@@ -0,0 +1,19 @@
+export interface Prompt {
+  id: string;
+  name: string;
+  displayName?: string;
+  originalName?: string;
+  gatewayId?: string | null;
+  gatewaySlug?: string | null;
+  description?: string | null;
+  tags?: Array<{ id: string; label: string }>;
+  arguments?: PromptArgument[];
+}
+
+export interface PromptArgument {
+  name: string;
+  description?: string;
+  required?: boolean;
+}
+
+export type PromptsResponse = Prompt[] | { prompts?: Prompt[] };
