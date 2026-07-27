bfbf2241fad3ca762074631558df8b8efdc95c62
Mohan Lakshmaiah <mohan.economist@gmail.com>
2026-07-27 11:21:01 +0530

    fix: reorder cpex import before mcpgateway imports in streamablehttp_transport
    Pylint C0411 requires third-party (cpex) imports before first-party
    (mcpgateway) imports. The cpex import is in the late-import section
    to avoid import cycles, so it gets both noqa E402 and pylint disable
    for both wrong-import-order and wrong-import-position.
    
    Signed-off-by: Mohan Lakshmaiah <mohan.economist@gmail.com>

--- mcpgateway/transports/streamablehttp_transport.py
+++ mcpgateway/transports/streamablehttp_transport.py
@@ -333,8 +333,8 @@ server_id_var: contextvars.ContextVar[str] = contextvars.ContextVar("server_id",
 # here for backwards-compat: external callers that already do
 # `from mcpgateway.transports.streamablehttp_transport import request_headers_var`
 # keep working.
+from cpex.framework import GlobalContext  # noqa: E402  # pylint: disable=wrong-import-order,wrong-import-position
 from mcpgateway.transports.context import request_headers_var, user_context_var, user_identity_var  # noqa: E402  # pylint: disable=wrong-import-position
-from cpex.framework import GlobalContext  # noqa: E402  # pylint: disable=wrong-import-position
 
 _oauth_checked_var: contextvars.ContextVar[bool] = contextvars.ContextVar("_oauth_checked", default=False)
 
